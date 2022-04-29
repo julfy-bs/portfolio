489a522946c6305003972b283d18d698fcfbe1aa
julfy-bs <julfy.web@gmail.com>
2022-04-29 19:38:31 +0300

    Updates

---
+++ js/chunk-2d0d7a19.ab8a03ac.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d7a19"],{7846:function(e,n,c){"use strict";c.r(n);var t=c("7a23"),o=Object(t["createElementVNode"])("h1",null,"reviews",-1),r=[o];function a(e,n,c,o,a,d){return Object(t["openBlock"])(),Object(t["createElementBlock"])("div",null,r)}var d={name:"AdminReviews"},i=c("6b0d"),l=c.n(i);const u=l()(d,[["render",a]]);n["default"]=u}}]);
+//# sourceMappingURL=chunk-2d0d7a19.ab8a03ac.js.map
---
+++ js/chunk-2d0f00de.b4895636.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f00de"],{"9b76":function(n,e,c){"use strict";c.r(e);var t=c("7a23");function o(n,e){return Object(t["openBlock"])(),Object(t["createElementBlock"])("h1",null," Блок «Обо мне» ")}var r=c("6b0d"),u=c.n(r);const a={},d=u()(a,[["render",o]]);e["default"]=d}}]);
+//# sourceMappingURL=chunk-2d0f00de.b4895636.js.map
